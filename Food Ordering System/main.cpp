b0963b2851318a23225c3aa019a23eeca7df2bd9
Salim Zia <Salimzia6@gmail.com>
2025-07-24 08:51:29 +0500

    Initial Commit

---
+++ Food Ordering System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Food Ordering System
+//
+//  Created by Salim Zia on 24/07/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
